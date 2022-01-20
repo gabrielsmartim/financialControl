b1958bdf1e668a0a03c1e58ecc0e9454d9fdac9a
Gabriel <martim.gabriel12@gmail.com>
2022-01-20 00:23:40 -0300

    firt commit

---
+++ src/components/TableArea/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+    width: 100%;
+    background-color: white;
+    padding: 20px;
+    box-shadow: 0 0 5px #CCC ;
+    border-radius: 10px;
+    margin-top: 20px;
+`;
+
+export const TableHeadColumn = styled.th<{width?: number}>`
+    padding: 10px 0;
+    text-align: left;
+    width: ${props => props.width ? `${props.width}px` : 'auto'};
+
+`;
---
+++ src/data/item.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/item';
+
+export const items: Item[] = [
+    { date: new Date(2022, 0, 5), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date(2022, 0, 5), category: 'food', title: 'Burguer King', value: 28},
+    { date: new Date(2022, 0, 15), category: 'rent', title: 'Aluguel APT', value: 2300},
+    { date: new Date(2022, 0, 15), category: 'salary', title: 'Salário ACME', value: 4500},
+];
